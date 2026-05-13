66e18749c1c7856a490fee1e377b8f1958175411
mauricio1804 <maumauaz18@gmail.com>
2026-05-13 13:41:12 -0300

    ajustes_info_dataset

--- info_dataset.py
+++ info_dataset.py
@@ -21,15 +21,23 @@ def audio_info(path):
 
 taxa_amostragem, duracao = audio_info(dataset_dir)
 
-print("Média duração: ", np.mean(duracao))
-print("Mínima duração: ", np.min(duracao))
-print("Máxima duração: ", np.max(duracao))
-print("Quantidade de arquivos: ", len(duracao))
-print("\nDuracão do áudio em segundos: \n", duracao)
+if (np.mean(duracao) == 2 and np.min(duracao) == 2 and np.max(duracao) == 2 and len(duracao) == 120):
+    print("Todos os 120 arquivos de áudio têm a duração correta de 2 segundos.")
+else:
+    print("Nem todos os arquivos de áudio têm a duração correta de 2 segundos. Verifique os dados.")
+    print("Média duração: ", np.mean(duracao))
+    print("Mínima duração: ", np.min(duracao))
+    print("Máxima duração: ", np.max(duracao))
+    print('valores únicos da duração: ', np.unique(duracao))
+    print("Quantidade de arquivos lidos: ", len(duracao))
 
 
-print("\nMédia da taxa de amostragem: ", np.mean(taxa_amostragem))
-print("Mínima taxa de amostragem: ", np.min(taxa_amostragem))
-print("Máxima taxa de amostragem: ", np.max(taxa_amostragem))
-print("Quantidade de arquivos: ", len(taxa_amostragem))
-print("\nTaxas de amostragem: \n", taxa_amostragem)
+if (np.mean(taxa_amostragem) == 44100 and np.min(taxa_amostragem) == 44100 and np.max(taxa_amostragem) == 44100 and len(taxa_amostragem) == 120):
+    print("Todos os 120 arquivos de áudio têm a taxa de amostragem correta de 44100 Hz.")
+else:
+    print("Nem todos os arquivos de áudio têm a taxa de amostragem correta de 44100 Hz. Verifique os dados.")
+    print("\nMédia da taxa de amostragem: ", np.mean(taxa_amostragem))
+    print("Mínima taxa de amostragem: ", np.min(taxa_amostragem))
+    print("Máxima taxa de amostragem: ", np.max(taxa_amostragem))
+    print("Valores únicos da taxa de amostragem: ", np.unique(taxa_amostragem))
+    print("Quantidade de arquivos lidos: ", len(taxa_amostragem))
